e43501348f5288a28db9b93a0e682197c2c92341
Unknown <originaldavz@gmail.com>
2018-01-10 10:04:08 +0000

    Add an empty list notice when there are no decks
    Also a button to switch to add deck tab

--- components/DeckList.js
+++ components/DeckList.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { fetchDecks } from '../utils/api';
 import { receiveDecks } from '../actions';
-import { gray } from '../utils/colours';
+import { gray, white, purple } from '../utils/colours';
 
 class DeckList extends Component {
 
@@ -19,21 +19,38 @@ class DeckList extends Component {
 		this.props.navigation.navigate('deckDetails', { key });
 	}
 
+	handleAddDeckPress = () => {
+		this.props.navigation.navigate('addDeck');
+	}
+
 	render() {
 		const { decks } = this.props;
+		const decksCount = Object.keys(decks).length;
 
 		return (
 			<View style={styles.container}>
-				{Object.keys(decks).map(key => {
-					const deck = decks[key];
+				{decksCount === 0
+					? (
+						<View>
+							<Text style={styles.emptyText}>There are currently no decks to review. Create a new deck and add some question cards now 😄</Text>
+							<TouchableOpacity onPress={this.handleAddDeckPress} style={styles.button}>
+								<Text style={styles.buttonText}>Add Deck</Text>
+							</TouchableOpacity>
+						</View>
+					)
+					: (
+						Object.keys(decks).map(key => {
+							const deck = decks[key];
 
-					return (
-						<TouchableOpacity onPress={() => this.handleDeckPress(key)} key={key} style={styles.deck}>
-							<Text style={styles.deckTitle}>{deck.title}</Text>
-							<Text style={styles.deckCount}>{deck.questions.length} cards</Text>
-						</TouchableOpacity>
-					);
-				})}
+							return (
+								<TouchableOpacity onPress={() => this.handleDeckPress(key)} key={key} style={styles.deck}>
+									<Text style={styles.deckTitle}>{deck.title}</Text>
+									<Text style={styles.deckCount}>{deck.questions.length} cards</Text>
+								</TouchableOpacity>
+							);
+						})
+					)
+				}
 			</View>
 		);
 	}
@@ -58,7 +75,29 @@ const styles = StyleSheet.create({
 	deckCount: {
 		color: gray,
 		fontSize: 16
-	}
+	},
+	emptyText: {
+		fontSize: 18,
+		textAlign: 'center',
+		paddingTop: 20,
+		paddingBottom: 40
+	},
+	button: {
+		flex: 1,
+		padding: 30,
+		width: '50%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		alignSelf: 'center',
+		marginBottom: 20,
+		borderRadius: 2,
+		backgroundColor: purple,
+		borderWidth: 1
+	},
+	buttonText: {
+		color: white,
+		fontSize: 20
+	},
 });
 
 function mapStateToProps(state) {
